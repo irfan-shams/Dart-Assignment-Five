5ec33e478f6193b21163800af9ff3c76f00d42b6
irfan-shams <shams.irfan@gmail.com>
2024-09-21 18:54:59 +0500

    Updated

--- factorial.dart
+++ factorial.dart
@@ -1,10 +1,12 @@
-/* Write a function that calculates the factorial of a number using a loop.
-For example, factorial of 5 (denoted as 5!) is 5 x 4 x 3 x 2 x 1 = 120. */
+// Write a function that calculates the factorial of a number using a loop.
+import 'dart:io';
+
 void main() {
-  int number = 7;
+  print("Enter number");
+  int number = int.parse(stdin.readLineSync()!);
   int result = 1;
-  for (int i = number; i >= 1; i--) {
-    result *= i;
+  for (int i = 1; i <= number; i++) {
+    result = result * i;
   }
   print("The factorial of $number is $result");
 }
